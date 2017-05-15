5b3ac451ed5c2d904b97ad5516654dacda2668a1
chuongntv <chuongntv.1409@gmail.com>
2017-05-15 21:22:04 +0700

    Add field to change secret key

--- openstack_dashboard/dashboards/settings/password/forms.py
+++ openstack_dashboard/dashboards/settings/password/forms.py
@@ -42,7 +42,7 @@ class PasswordForm(forms.SelfHandlingForm):
     confirm_password = forms.CharField(
         label=_("Confirm new password"),
         widget=forms.PasswordInput(render_value=False))
-    new_secret_key = forms.CharField(
+    new_secret_key = forms.CheckboxInput(
         label=_("New Secret Key"),
         widget=forms.CheckboxInput(render_value=False))
     no_autocomplete = True
